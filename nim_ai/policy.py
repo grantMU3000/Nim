36b08eb2f047185dcf4d52ebdcc1b5f7ecb01f20
Grant <robinsonjg64@gmail.com>
2025-10-31 17:37:41 -0400

    Made AI make a move

--- nim_ai/policy.py
+++ nim_ai/policy.py
@@ -10,6 +10,26 @@ class TrainedPolicy:
                 self.model = pickle.load(f)
     
     """
-    AI will choose a move.
+    AI will choose a move. (Model is trained, so no need to worry about epsilon)
     """
     def choose_move(self, state: GameState) -> Move:
+        bestAction = ()
+        statePiles = state.piles
+        stateTuple = tuple(statePiles)
+        bestQ = -1
+
+        # Adding available actions to the list, and keeping track of best action
+        for i in range(len(statePiles)):
+            pileSize = state[i]
+            for j in range(1, pileSize + 1):
+                action = (i, j)
+                qVal = 0
+                # Accessing a state/action pair's q-value
+                if (stateTuple, action) in self.model.q:
+                    qVal = self.model.q[(stateTuple, action)]
+                # Updating best action if necessary
+                if qVal >= bestQ:
+                    bestAction = (action)
+                    bestQ = qVal
+        
+        return bestAction
